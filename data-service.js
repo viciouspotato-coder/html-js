8458bde199cf1257ee94ff8ea8e013eb47bd0063
Henry Tao <taohenry1999@gmail.com>
2021-11-19 10:52:04 -0500

    assignment 5

---
+++ data-service.js
@@ -0,0 +1,272 @@
+const server = require("./server.js");
+const Sequelize = require('sequelize');
+
+var fs = require("fs");
+
+var sequelize = new Sequelize('d64nrdsl25rluv', 'wlrrdhlextwwta', '0b202b79a04fe81e83a16c2e5d7a4af68f413462ffa5fd2dba5cb2d5013922c0', {
+    host: 'ec2-34-203-114-67.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    port: 5432,
+    dialectOptions: {
+        ssl: { rejectUnauthorized: false }
+    },
+    query: { raw: true }
+});
+
+const Employee = sequelize.define('Employee', {
+    employeeNum: {
+        type: Sequelize.INTEGER,
+        primaryKey: true, // use "project_id" as a primary key
+        autoIncrement: true // automatically increment the value
+    },
+    firstName: Sequelize.STRING,
+    lastName: Sequelize.STRING,
+    email: Sequelize.STRING,
+    SSN: Sequelize.STRING,
+    addressStreet: Sequelize.STRING,
+    addressCity: Sequelize.STRING,
+    addressState: Sequelize.STRING,
+    addressPostal: Sequelize.STRING,
+    maritalStatus: Sequelize.STRING,
+    isManager: Sequelize.BOOLEAN,
+    employeeManagerNum: Sequelize.INTEGER,
+    status: Sequelize.STRING,
+    hireDate: Sequelize.STRING
+});
+
+const Department = sequelize.define('Department', {
+    departmentId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true, // use "project_id" as a primary key
+        autoIncrement: true // automatically increment the value
+    },
+    departmentName: Sequelize.STRING
+});
+
+Department.hasMany(Employee, { foreignKey: 'department' });
+
+function initialize() {
+    return new Promise(function (resolve, reject) {
+        sequelize.sync().then(() => {
+            resolve();
+        }).catch(() => {
+            reject("unable to sync the database"); //return;
+        });
+    });
+}
+
+function getAllEmployees() {
+    return new Promise(function (resolve, reject) {
+        Employee.findAll().then((data) => {
+            resolve(data);
+        }).catch(() => {
+            reject("no results returned");
+        });
+    });
+}
+
+function getEmployeesByStatus(status) {
+    return new Promise(function (resolve, reject) {
+        Employee.findAll({
+            where: { status: status }
+        }).then(function (data) {
+            resolve(data);
+        }).catch(() => {
+            reject("no results returned");
+        });
+    });
+}
+
+function getEmployeesByDepartment(department) {
+    return new Promise(function (resolve, reject) {
+        Employee.findAll({
+            where: { department: department }
+        }).then(function (data) {
+            resolve(data);
+        }).catch(() => {
+            reject("no results returned");
+        });
+    });
+}
+
+function getEmployeesByManager(manager) {
+    return new Promise(function (resolve, reject) {
+        Employee.findAll({
+            where: { employeeManagerNum: manager }
+        }).then(function (data) {
+            resolve(data);
+        }).catch(() => {
+            reject("no results returned");
+        });
+    });
+}
+
+function getEmployeeByNum(num) {
+    return new Promise(function (resolve, reject) {
+        Employee.findAll({
+            where: { employeeNum: num }
+        }).then(function (data) {
+            resolve(data);
+        }).catch(() => {
+            reject("no results returned");
+        });
+    });
+}
+
+function getManagers() {
+    return new Promise(function (resolve, reject) {
+        Employee.findAll({
+            where: { isManager: true }
+        }).then(function (data) {
+            resolve(data);
+        }).catch(() => {
+            reject("no results returned");
+        });
+    });
+}
+
+function getDepartments() {
+    return new Promise(function (resolve, reject) {
+        Department.findAll().then(function (data) {
+            resolve(data);
+        }).catch(() => {
+            reject("no results returned");
+        });
+    });
+}
+
+function addEmployee(employeeData) {
+    return new Promise(function (resolve, reject) {
+        return new Promise(function (resolve, reject) {
+            employeeData.isManager = (employeeData.isManager) ? true : false;
+            for (i in employeeData) {
+                if (employeeData[i] == "") {
+                    employeeData[i] = null;
+                }
+            }
+            resolve();
+        }).then(() => {
+            console.log(employeeData)
+            Employee.create(employeeData).then(() => {
+                resolve();
+            }).catch(() => {
+                reject("unable to create employee");
+            });
+        });
+    });
+}
+
+function updateEmployee(employeeData) {
+    return new Promise(function (resolve, reject) {
+        return new Promise(function (resolve, reject) {
+            employeeData.isManager = (employeeData.isManager) ? true : false;
+            for (i in employeeData) {
+                if (employeeData[i] == "") {
+                    employeeData[i] = null;
+                }
+            }
+            resolve();
+        }).then(() => {
+            Employee.update({
+                employeeNum: employeeData.employeeNum,
+                firstName: employeeData.firstName,
+                lastName: employeeData.lastName,
+                email: employeeData.email,
+                SSN: employeeData.SSN,
+                addressStreet: employeeData.addressStreet,
+                addressCity: employeeData.addressCity,
+                addressState: employeeData.addressState,
+                addressPostal: employeeData.addressPostal,
+                maritalStatus: employeeData.maritalStatus,
+                isManager: employeeData.isManager,
+                employeeManagerNum: employeeData.employeeManagerNum,
+                status: employeeData.status,
+                department: employeeData.department,
+                hireDate: employeeData.hireDate
+            }, {
+                where: { employeeNum: employeeData.employeeNum }
+            }).then(() => {
+                console.log("successfully updated employee: " + employeeData.employeeNum);
+                resolve()
+            }).catch(() => {
+                reject("unable to update employee");
+            });
+        });
+    });
+}
+
+function deleteEmployeeByNum(empNum) {
+    return new Promise(function (resolve, reject) {
+        Employee.destroy({
+            where: { employeeNum: empNum }
+        }).then(() => {
+            console.log("successsfully removed employee: ", empNum);
+            resolve();
+        }).catch((err) => {
+            console.log(err);
+            reject("unable to delete employee");
+        });
+    });
+}
+
+function addDepartment(departmentData) {
+    return new Promise(function (resolve, reject) {
+        if (departmentData.departmentName == "") {
+            departmentData.departmentName = NULL;
+        }
+        Department.create(departmentData)
+            .then((data) => {
+                resolve(data);
+            }).catch(() => {
+                reject("unable to create department");
+            });
+    });
+}
+
+function updateDepartment(departmentData) {
+    return new Promise(function (resolve, reject) {
+        for (const i in departmentData) {
+            if (i == "") {
+                i = NULL;
+            }
+        }
+        Department.update({
+            departmentId: departmentData.departmentId,
+            departmentName: departmentData.departmentName
+        }, {
+            where: { departmentId: departmentData.departmentId }
+        }).then(() => {
+            console.log("successfully updated department: " + departmentData.departmentId);
+            resolve()
+        }).catch(() => {
+            reject("unable to update department");
+        });
+    });
+}
+
+function getDepartmentById(id) {
+    return new Promise(function (resolve, reject) {
+        Department.findOne({
+            where: { departmentId: id }
+        }).then(function (data) {
+            resolve(data);
+        }).catch(() => {
+            reject("no results returned");
+        });
+    });
+}
+
+function deleteDepartmentById(id) {
+    return new Promise(function (resolve, reject) {
+        Department.destroy({
+            where: { departmentId: id }
+        }).then(() => {
+            console.log("successsfully removed department: ", id);
+            resolve();
+        }).catch((err) => {
+            reject("unable to delete department");
+        });
+    });
+}
+
+module.exports = { fs, Employee, Department, initialize, getAllEmployees, getManagers, getDepartments, addEmployee, getEmployeesByStatus, getEmployeesByDepartment, getEmployeesByManager, getEmployeeByNum, updateEmployee, deleteEmployeeByNum, addDepartment, updateDepartment, getDepartmentById, deleteDepartmentById };
---
+++ server.js
@@ -0,0 +1,279 @@
+/*********************************************************************************
+ *  WEB322 – Assignment 4
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
+ *  No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites) or distributed to other students.
+ *
+ *  Name: Henry Tao Student ID: 118375203 Date: 11/05/2021
+ *
+ *  Online (Heroku) URL: https://peaceful-mesa-24709.herokuapp.com/
+ *
+ ********************************************************************************/
+
+var express = require("express");
+var multer = require('multer');
+var app = express();
+var path = require("path");
+var fs = require("fs");
+const data = require("./data-service.js");
+const exphbs = require('express-handlebars');
+const { homedir } = require("os");
+// const Sequelize = require('sequelize');
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+const storage = multer.diskStorage({
+  destination: "./public/images/uploaded",
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
+
+// call this function after the http server starts listening for requests
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+app.engine(".hbs", exphbs({
+  extname: ".hbs",
+  defaultLayout: 'main',
+  helpers: {
+    navLink: function (url, options) {
+      return '<li' +
+        ((url == app.locals.activeRoute) ? ' class="active" ' : '') +
+        '><a href="' + url + '">' + options.fn(this) + '</a></li>';
+    },
+    equal: function (lvalue, rvalue, options) {
+      if (arguments.length < 3)
+        throw new Error("Handlebars Helper equal needs 2 parameters");
+      if (lvalue != rvalue) {
+        return options.inverse(this);
+      } else {
+        return options.fn(this);
+      }
+    }
+  }
+}));
+app.set("view engine", ".hbs");
+
+app.use(express.urlencoded({ extended: true }))
+app.use(function (req, res, next) {
+  let route = req.baseUrl + req.path;
+  app.locals.activeRoute = (route == "/") ? "/" : route.replace(/\/$/, "");
+  next();
+});
+
+app.use(express.static("public"));
+
+// setup a 'route' to listen on the default url path (http://localhost)
+app.get("/", function (req, res) {
+  // res.sendFile(path.join(__dirname, "/views/home.html"));
+  res.render('home');
+});
+
+// setup another route to listen on /about
+app.get("/about", function (req, res) {
+  res.render('about');
+});
+
+app.get("/employees", function (req, res) {
+  if (req.query.status) {
+    data.getEmployeesByStatus(req.query.status).then((result) => {
+      if (result.length > 0) {
+        res.render('employees', { result });
+      } else {
+        res.render("employees", { message: "no results" });
+      }
+    }).catch(() => {
+      res.render("employees", { message: "no results" });
+    })
+  } else if (req.query.department) {
+    data.getEmployeesByDepartment(req.query.department).then((result) => {
+      if (result.length > 0) {
+        res.render('employees', { result });
+      } else {
+        res.render("employees", { message: "no results" });
+      }
+    }).catch(() => {
+      res.render("employees", { message: "no results" });
+    })
+  } else if (req.query.manager) {
+    data.getEmployeesByManager(req.query.manager).then((result) => {
+      if (result.length > 0) {
+        res.render('employees', { result });
+      } else {
+        res.render("employees", { message: "no results" });
+      }
+    }).catch(() => {
+      res.render("employees", { message: "no results" });
+    })
+  } else {
+    data.getAllEmployees().then((result) => {
+      if (result.length > 0) {
+        res.render('employees', { result });
+      } else {
+        res.render("employees", { message: "no results" });
+      }
+    }).catch(() => {
+      res.render("employees", { message: "no results" });
+    })
+  }
+});
+
+app.get("/employee/:empNum", (req, res) => {
+
+  // initialize an empty object to store the values
+  let viewData = {};
+
+  data.getEmployeeByNum(req.params.empNum).then((result) => {
+    if (result) {
+      viewData.employee = result; //store employee data in the "viewData" object as "employee"
+    } else {
+      viewData.employee = null; // set employee to null if none were returned
+    }
+  }).catch(() => {
+    viewData.employee = null; // set employee to null if there was an error 
+  }).then(data.getDepartments)
+    .then((result) => {
+      viewData.departments = result; // store department data in the "viewData" object as "departments"
+
+      // loop through viewData.departments and once we have found the departmentId that matches
+      // the employee's "department" value, add a "selected" property to the matching 
+      // viewData.departments object
+      for (let i = 0; i < viewData.departments.length; i++) {
+        if (viewData.departments[i].departmentId == viewData.employee[0].department) {
+          viewData.departments[i].selected = true;
+        }
+      }
+    }).catch(() => {
+      viewData.departments = []; // set departments to empty if there was an error
+    }).then(() => {
+      if (viewData.employee == null) { // if no employee - return an error
+        res.status(404).send("Employee Not Found");
+      } else {
+        res.render("employee", { viewData: viewData }); // render the "employee" view
+      }
+    });
+});
+
+
+
+app.get("/employees/add", function (req, res) {
+  data.getDepartments().then((result) => {
+    res.render("addEmployee", { departments: result });
+  }).catch(() => {
+    res.render("addEmployee", { departments: [] });
+  })
+});
+
+app.post("/employees/add", function (req, res) {
+  data.addEmployee(req.body).then(() => {
+    res.redirect("/employees");
+  }).catch(() => {
+    res.redirect("/employees");
+  })
+});
+
+app.post("/employee/update", (req, res) => {
+  data.updateEmployee(req.body).then(() => {
+    res.redirect("/employees");
+  }).catch(() => {
+    console.log();
+  })
+});
+
+app.get(["/employees/delete/:empNum"], function (req, res) {
+  data.deleteEmployeeByNum(req.params.empNum).then(() => {
+    res.redirect("/employees");
+  }).catch(() => {
+    res.status(500).send("Unable to Remove Employee / Employee not found");
+  })
+});
+
+app.get("/managers", function (req, res) {
+  data.getManagers().then((result) => {
+    res.render('managers', { result });
+  }).catch(() => {
+    res.render("managers", { message: "no results" });
+  })
+});
+
+app.get("/departments", function (req, res) {
+  data.getDepartments().then((result) => {
+    if (result.length > 0) {
+      console.log(result);
+      res.render('departments', { result });
+    } else {
+      res.render("departments", { message: "no results" });
+    }
+  }).catch(() => {
+    res.render("departments", { message: "no results" });
+  })
+});
+
+app.get("/departments/add", function (req, res) {
+  res.render('addDepartment');
+});
+
+app.post("/departments/add", function (req, res) {
+  data.addDepartment(req.body).then(() => {
+    res.redirect("/departments");
+  }).catch(() => {
+    res.redirect("/departments");
+  })
+});
+
+app.post("/departments/update", (req, res) => {
+  data.updateDepartment(req.body).then(() => {
+    res.redirect("/departments");
+  }).catch(() => {
+    console.log();
+  })
+});
+
+app.get(["/departments/:departmentId"], function (req, res) {
+  data.getDepartmentById(req.params.departmentId).then((result) => {
+    res.render('department', { result });
+  }).catch(() => {
+    res.status(404).send("Department Not Found");
+  })
+});
+
+app.get(["/departments/delete/:departmentId"], function (req, res) {
+  data.deleteDepartmentById(req.params.departmentId).then((result) => {
+    res.redirect("/departments");
+  }).catch(() => {
+    res.status(500).send("Unable to Remove Department / Department not found");
+  })
+});
+
+app.get("/images/add", function (req, res) {
+  res.render('addImage');
+});
+
+app.post("/images/add", upload.single("imageFile"), function (req, res) {
+  res.redirect("/images");
+})
+
+app.get("/images", function (req, res) {
+  fs.readdir("./public/images/uploaded", function (err, files) {
+    var image = { "images": files };
+    res.render("images", image);
+  });
+
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(__dirname + "/404.html");
+});
+
+// setup http server to listen on HTTP_PORT
+data.initialize()
+  .then((message) => {
+    app.listen(HTTP_PORT, onHttpStart);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
